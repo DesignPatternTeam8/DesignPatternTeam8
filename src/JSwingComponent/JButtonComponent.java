2f7e1841d0b2cbe511c91e61e98c72c7d00ae060
Ho-il Lee <hi251kk@gmail.com>
2015-12-01 20:53:02 +0900

    Observable 가능하

--- src/JSwingComponent/JButtonComponent.java
+++ src/JSwingComponent/JButtonComponent.java
@@ -1,11 +1,16 @@
 package JSwingComponent;
 
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JButtonComponent extends JButton {
+public class JButtonComponent extends JButtonObservable implements ActionListener {
 	public JButtonComponent(JSwingComponentProperties componentProperties) {
 
 		this.setText(componentProperties.getText());
 
+	}
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		
 	}
 }
---
+++ src/JSwingComponent/JButtonObservable.java
@@ -0,0 +1,24 @@
+package JSwingComponent;
+
+import javax.swing.JButton;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class JButtonObservable extends JButton implements Observable {
+
+	@Override
+	public void addListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
