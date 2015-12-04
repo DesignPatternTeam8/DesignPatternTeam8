8e5ad169724d9b3a79cbc25679a99d314799f5a4
Jaegwan <kjg5417@gmail.com>
2015-12-04 16:40:24 +0900

    Revert "db_observer"
    This reverts commit 34b19da63644e25f3645f791554dca05929fd16e.

--- src/GUI/Test_Oberver.java
+++ src/GUI/Test_Oberver.java
@@ -2,13 +2,13 @@ package GUI;
 import java.util.Observable;
 import java.util.Observer;
 
-public class DB_Oberver implements Observer {
+public class Test_Oberver implements Observer {
 //    private Member mem = new Member();
 //    public Test_Oberver(Member mem) {
 //        this.mem = mem;
 //    }
     public void update(Observable o, Object arg) {
-        if (o instanceof Observable) {
+        if (o instanceof Test_Observable) {
             
         }
     }
